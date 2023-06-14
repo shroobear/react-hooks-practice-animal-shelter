d3af8680c9f2e541229e613e794e7f16580bfc81
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-06-13 21:02:44 -0400

    Lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,29 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  const handleChangeType = (e) => {
+    setFilters({ type: e.target.value });
+  };
+
+  function handleFindPetsClick() {
+    let url = "http://localhost:3001/pets";
+
+    if (filters.type !== "all") {
+      url += `?type=${filters.type}`
+    }
+
+    fetch(url)
+      .then((r) => r.json())
+      .then((data) => setPets(data))
+      .catch((error) => console.log(error));
+  }
+
+  const handleAdoptPet = (id) => {
+    setPets((prevPets) =>
+      prevPets.map((pet) => (pet.id === id ? { ...pet, isAdopted: true } : pet))
+    );
+  };
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +38,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={handleChangeType} onFindPetsClick={handleFindPetsClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
           </div>
         </div>
       </div>
